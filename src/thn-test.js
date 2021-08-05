88f61849b9b01692949f85425369323eccf22e4a
Irene Perez Alonso <irperal@hotmail.com>
2021-08-05 17:30:09 +0200

    Delete comments in code

--- src/thn-test.js
+++ src/thn-test.js
@@ -2,14 +2,6 @@ function retrieveAvaliabilityRooms() {
 
     const hotelData = {};
 
-    //Customer data (number of adults, children, dates ...)
-    const summaryData = document.querySelector('#fb-qs-summary');
-    //All the search results (rooms)
-    const mainContainer = document.querySelector('#results-items');
-    //One of the results within all the results
-    const searchResults = mainContainer.querySelectorAll('.fb-results-accommodation');
-
-
     function getCheckDates() {
 
         const arrivalDate = document.querySelector('#fb-qs-summary-dates-arrival > span');
@@ -57,62 +49,6 @@ function retrieveAvaliabilityRooms() {
 
         hotelData.language = lenguage;
       };
-
-    // function getRoomsInfo(roomsTypes) {
-    //     const priceList = [];
-    //     const nights = document.querySelector('').getAttribute('');
-
-    //     for(let i = 0; i < roomsTypes.lenght; i += 1) {
-    //         for(let n = 0; n < roomsTypes[i].prices.lenght; z += 1) {
-    //             priceList.push({
-    //                 price: roomsTypes[i].prices[n].toFixed(2),
-    //                 dailyPrice: ((roomsTypes[i].prices[n]) / nights).toFixed(2),
-    //                 refundable: !roomsTypes[i].refundable[n].includes('cross'),
-    //                 breakfast: !roomsTypes[i].breakfast[n].includes('cross'),
-    //                 typeRoom: roomsTypes[i].type,
-    //             });
-    //         }
-    //     }
-
-    //     hotelData.rateList = priceList;
-    //     priceList.sort((a, b) => a.price - b.price);
-    //     hotelData.minimumPrice = { ...priceList[0] };
-        
-
-    // }
-
-    // function getRoomsData() {
-    //     const roomsByTypes = [];
-    //     const roomsSelection = document.getElementsByClassName('');
-
-    //     for (let i = 0; i < roomsSelection.length; i += 1) {
-
-    //       const pricesSelection = roomsSelection[i].querySelectorAll('');
-    //       const pricesFiltered = Array.from(pricesSelection).filter((element) => (element.getAttribute('class') === ''));
-    //       const pricesArray = pricesFiltered.map((element) => Number(element.getAttribute('')));
-    //       const refundable = Array.from(roomsSelection[i].querySelectorAll('')).map((element) => (element.getAttribute('')));
-    //       const breakfast = Array.from(roomsSelection[i].querySelectorAll('')).map((element) => (element.getAttribute('')));
-    //       roomsByTypes.push({
-    //         type: (roomsSelection[i].querySelector('')).getAttribute(''),
-    //         prices: pricesArray,
-    //         refundable,
-    //         breakfast,
-    //       });
-    //     }
-
-    //     getRoomsDetails(roomsByTypes);
-    //   };
-
-    // function getGuestsNumber() {
-    //     const selectionRoom = document.querySelector('');
-    //     hotelData.rooms = Number(selectionRoom.getAttribute('').replace());
-
-    //     const adults = Number(document.querySelector('').getAttribute(''));
-    //     const children = Number(document.querySelector('').getAttribute(''));
-
-    //     hotelData.guests = { adults, children };
-    // }
-
 
     return hotelData;
 }
